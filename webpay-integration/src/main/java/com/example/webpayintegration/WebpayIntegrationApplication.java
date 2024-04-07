767c6256755df6336805519cdc9f3d6ce321e69d
Juan Claudio Valenzuela <jvalenzuelaortega@gmail.com>
2024-04-06 23:31:31 -0300

    FEATURE: base structure

---
+++ webpay-integration/src/main/java/com/example/webpayintegration/WebpayIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpayintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpayIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpayIntegrationApplication.class, args);
+	}
+
+}
---
+++ webpay-integration/src/test/java/com/example/webpayintegration/WebpayIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpayintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpayIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
